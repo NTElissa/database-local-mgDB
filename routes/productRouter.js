fd18175fcd1580076a9d94c92dfe4e6417489a3c
NTElissa <ntihindukaelissa77@gmail.com>
2024-10-16 20:02:22 +0200

    new update

--- routes/productRouter.js
+++ routes/productRouter.js
@@ -7,23 +7,17 @@ import {
   updateProductController,
   deleteProductController
 } from '../controllers/productControllers.js';
-// import { protect, admin } from "../middleware/userMiddleware.js";
+
 
 const productRouter = express.Router();
-
-// Create a product (Admin only)
 productRouter.post('/products', createProductController);
 
-// Get all products (Public)
 productRouter.get('/products', getAllProductsController);
 
-// Get a product by ID (Public)
 productRouter.get('/products/:id',  getProductController);
 
-// Update a product by ID (Admin only)
 productRouter.put('/products/:id',  updateProductController);
 
-// Delete a product by ID (Admin only)
 productRouter.delete('/products/:id',  deleteProductController);
 
 export default productRouter;
