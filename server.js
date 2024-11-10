a3c18e35cef5063903a412c02571ba435ef92497
NTElissa <ntihindukaelissa77@gmail.com>
2024-11-10 11:03:19 +0200

    hello

--- server.js
+++ server.js
@@ -2,8 +2,13 @@
 import express from 'express';
 import connectDB from './app.js';
 import productRouter from './routes/productRouter.js';
+import cors from 'cors'; //its help us to coonect deffrent port for fontend side and backecnd side 
+
+
+
 
 const app = express();
+ app.use(cors());
 const PORT = 8000;
 
 // Middleware to parse JSON
